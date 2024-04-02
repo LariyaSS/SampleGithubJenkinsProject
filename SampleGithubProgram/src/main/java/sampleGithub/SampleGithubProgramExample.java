63db21706d7d37967ee7f7be27fc36cdc2ebd7b5
lariy <lariy@HP>
2024-04-02 12:34:16 +0530

    sample program commit eclipse ide 2021-12

---
+++ SampleGithubProgram/src/main/java/sampleGithub/SampleGithubProgramExample.java
@@ -0,0 +1,10 @@
+package sampleGithub;
+
+public class SampleGithubProgramExample {
+
+	public static void main(String[] args) {
+		System.out.println("sample Github and jenkins program");
+
+	}
+
+}
